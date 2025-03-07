bc01469b8b12f40a29cd17925d41358380d07223
Gansemeran <gansemeran@gmail.com>
2025-03-07 17:29:08 +1100

    hyprlock + starting vim

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
 	config = function()
 		require('telescope').setup({})
 		local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>pf', builtin.find_files)
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
@@ -16,8 +16,11 @@ return {
             builtin.grep_string({ search = word })
         end)
         vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+			builtin.live_grep()
         end)
+		vim.keymap.set('n', '<leader>pS', function ()
+			builtin.grep_string({ search = vim.fn.input("Grep > ") })
+		end)
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
     end
 
--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,8 +1,8 @@
 function SetTheme()
-	color = "rose-pine-moon"
+	local color = "rose-pine-moon"
 	vim.cmd.colorscheme(color)
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
@@ -11,10 +11,10 @@ return {
         name = "rose-pine",
         config = function()
             require('rose-pine').setup({
-                disable_background = true,
-                styles = {
-                    italic = false,
-                },
+                --disable_background = false,
+                --styles = {
+					--italic = false,
+                --},
             })
 
             SetTheme();
