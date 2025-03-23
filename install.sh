aec0d0a17145f5fd6f282f7e83a656be1cda0278
Gansemeran <gansemeran@gmail.com>
2025-03-23 14:30:50 +1100

    install p10k

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ install() {
 
 # omz
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
 # fzf
 install fzf tmux stow
