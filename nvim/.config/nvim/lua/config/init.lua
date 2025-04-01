cdef3cac9f68049658224d42b3a8bd7089c5bbb9
Gansemeran <gansemeran@gmail.com>
2025-04-01 18:07:17 +1100

    add lsp mappings

--- nvim/.config/nvim/lua/config/init.lua
+++ nvim/.config/nvim/lua/config/init.lua
@@ -62,6 +62,8 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({"n", "v"}, "<leader>d", "\"_d")
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !~/tmux-session<CR>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
@@ -70,8 +72,32 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
+local augroup = vim.api.nvim_create_augroup
+local personal = augroup('personal', {})
 
+local autocmd = vim.api.nvim_create_autocmd
 
+autocmd({"BufWritePre"}, {
+    group = personal,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
 
+autocmd('LspAttach', {
+    group = personal,
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    end
+})
 
 require("config.lazy")
