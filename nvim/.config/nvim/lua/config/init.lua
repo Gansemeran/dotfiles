7c4726755ed5518db54f2506de8b0856bf036724
Gansemeran <gansemeran@gmail.com>
2025-04-01 18:18:51 +1100

    update deps + configs

--- nvim/.config/nvim/lua/config/init.lua
+++ nvim/.config/nvim/lua/config/init.lua
@@ -7,7 +7,7 @@ vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 
 
--- vim.opt 
+-- vim.opt
 
 vim.opt.guicursor = ""
 
@@ -41,7 +41,7 @@ vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
 
-
+vim.diagnostic.config({ virtual_text = true })
 
 -- vim.keymaps
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
@@ -91,13 +91,15 @@ autocmd('LspAttach', {
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
         vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set({"n", "v"}, "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
         vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
         vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "<leader>vri", function() vim.lsp.buf.implementation() end, opts)
+        vim.keymap.set("i", "<C-s>", function() vim.lsp.buf.signature_help() end, opts)
         vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
         vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
     end
 })
 
 require("config.lazy")
+
